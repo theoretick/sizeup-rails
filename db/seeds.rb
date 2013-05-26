8b12286c7b8d9d5cb19d3929fae4758c6125d02d
Duder-Onomy <greglarrenaga@gmail.com>
2013-05-26 12:45:54 -0700

    Fixed and populated db:seed file. The file first deletes everything in the DB before is seeds the DB.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Building.delete_all
+City.delete_all
+
+City.create([{name: 'Paris', country: 'France', id: '1'}])
+City.create([{name: 'Los Angeles', country: 'USA', id: '2'}])
+City.create([{name: 'Portland', country: 'USA', id: '3'}])
+
+Building.create([{ name: 'Eiffel Tower', height: '100', city_id: '1'}])
+Building.create([{ name: 'Tall Boy Tower', height: '350', city_id: '2'}])
+Building.create([{ name: 'Beer Heights', height: '275', city_id: '3'}])
