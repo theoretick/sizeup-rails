c019bf263a669620b18ed29c02bf33af7b2412ef
theoretick <mail@limitlesschannels.com>
2013-05-30 13:05:51 -0700

    added address parameter to buildings

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -101,6 +101,6 @@ class BuildingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def building_params
-      params.require(:building).permit(:name, :height, :city_id, :view, :gargoyle)
+      params.require(:building).permit(:name, :height, :city_id, :view, :gargoyle, :address)
     end
 end
--- app/models/building.rb
+++ app/models/building.rb
@@ -3,9 +3,9 @@ class Building < ActiveRecord::Base
 	validates :height, presence: true, numericality: true
 	belongs_to :city
 	
-    # method for CSV Import, take a file variable (csv file object) 
-    # the headers parameter tells the CSV object that the first row contains the headers
-    # Headers are the key's to be used in the hash-per-row building creation.
+  # method for CSV Import, take a file variable (csv file object) 
+  # the headers parameter tells the CSV object that the first row contains the headers
+  # Headers are the key's to be used in the hash-per-row building creation.
 	def self.import(file)
 	  CSV.foreach(file.path, headers: true) do |row|
 	    Building.create! row.to_hash
